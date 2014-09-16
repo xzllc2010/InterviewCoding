c033d9434a23a24a76ec4365356c778f018fc9b1
xzllc2010 <xzllc2010@sohu.com>
2014-09-16 22:27:30 +0800

    na

--- src/linkedlist/ListNodeMain.java
+++ src/linkedlist/ListNodeMain.java
@@ -1,30 +1,136 @@
 package linkedlist;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * Created by never on 14-9-14.
  */
 public class ListNodeMain {
 
     public static void main(String[] args) {
-        ListNode headNode = initLinkedList();
+        ListNode headNode = initList();
+        printListReverse(headNode);
 
-        ReverseLinkedList rll = new ReverseLinkedList();
-        ListNode tmpNode = rll.reverse(headNode);
-        PrintLinkedList(tmpNode);
     }
 
-    public static void PrintLinkedList(ListNode headNode) {
+    public static void printListReverse(ListNode head){
+        if(head == null){
+            return;
+        }
+
+        Stack<ListNode> stack = new Stack<ListNode>();
+        while (head != null){
+            stack.add(head);
+            head = head.next;
+        }
+        while (!stack.isEmpty()){
+            System.out.println(stack.pop().val);
+        }
+    }
+
+    public static ListNode getMiddleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }else if(head.next.next == null){
+            return head.next;
+        }
+
+        ListNode slowPoint = head;
+        ListNode fastPoint = head;
+
+        while (fastPoint.next != null) {
+            fastPoint = fastPoint.next;
+            slowPoint = slowPoint.next;
+
+            if(fastPoint.next != null){
+                fastPoint = fastPoint.next;
+            }
+        }
+        return slowPoint;
+    }
+
+    public static ListNode getKthNode(ListNode head, int k) {
+        if (k < 1 || k > getListLength(head) || head == null) {
+            return null;
+        }
+        ListNode slowPoint = head;
+        ListNode fastPoint = head;
+
+        while (k > 0) {
+            fastPoint = fastPoint.next;
+            k--;
+        }
+
+        while (fastPoint != null) {
+            fastPoint = fastPoint.next;
+            slowPoint = slowPoint.next;
+        }
+
+        return slowPoint;
+
+    }
+
+    public static ListNode reverseListRec(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode secondNode = head.next;
+        head.next = null;
+        ListNode reverseRest = reverseListRec(secondNode);
+        secondNode.next = head;
+        return reverseRest;
+    }
+
+    public static ListNode reverseList(ListNode head) {
+
+        if (head == null) {
+            return head;
+        }
+
+        ListNode preNode = head;
+        ListNode curNode = head.next;
+        ListNode nextNode = null;
+
+        while (curNode != null) {
+            nextNode = curNode.next;
+            curNode.next = preNode;
+            preNode = curNode;
+            curNode = nextNode;
+        }
+        head.next = null;
+        return preNode;
+
+    }
+
+    public static int getListLength(ListNode head) {
+        int lenOfList = 0;
+        if (head == null) {
+            return lenOfList;
+        }
+
+        while (head != null) {
+            lenOfList++;
+            head = head.next;
+        }
+        return lenOfList;
+    }
+
+    public static void displayList(ListNode headNode) {
         if (headNode == null) {
             System.out.print("The linked list is empty!");
         } else {
             while (headNode != null) {
-                System.out.printf(headNode.val + " ");
+                System.out.print(headNode.val + " ");
                 headNode = headNode.next;
             }
         }
     }
 
-    public static ListNode initLinkedList() {
+    public static ListNode initList() {
         ListNode l1 = new ListNode(1);
         ListNode l2 = new ListNode(2);
         ListNode l3 = new ListNode(3);
@@ -39,7 +145,7 @@ public class ListNodeMain {
         l4.next = l5;
         l5.next = l6;
         l6.next = l7;
-        l7.next = l8;
+        //l7.next = l8;
 
         return l1;
     }
